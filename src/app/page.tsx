7bad0be8717e9e19ea5a65bf3813426170c4af9c
imzaak <zakirbeyan2019@gmail.com>
2025-07-19 01:32:47 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of our application.</p>
-      <p>Feel free to explore the features and functionalities we offer.</p>
+     
     </div>
   );
 }
