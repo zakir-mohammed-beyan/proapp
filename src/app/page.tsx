26d9f29e4c7a081345aa90e2a8e996ac9f72adb3
imzaak <zakirbeyan2019@gmail.com>
2025-07-19 01:35:56 +0300

    Just updated some files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ const Home = () => {
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of our application.</p>
+      <p>Feel free to explore the features and functionalities we offer.</p>
+      <p>Stay tuned for more updates!</p>
      
     </div>
   );
