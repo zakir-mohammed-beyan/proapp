f49f9c5f0fe3679c79a81e1eda3824102804508e
imzaak <zakirbeyan2019@gmail.com>
2025-07-19 01:38:42 +0300

    Added a new header of project

---
+++ src/app/header.jsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Application Header</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
+// src/app/header.jsx
